ea52cbb375d4f6235f45de51ef638bf473b3c219
Paul Turner <pgturn68@gmail.com>
2021-06-02 20:20:06 -0500

    Update connection.js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-
-import Sequelize from 'sequelize';
+const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
